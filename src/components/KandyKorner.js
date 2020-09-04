0e5a99e35ab03f066921f852d03751b2eb6284a5
Jacob Eckert <jweckert17@gmail.com>
2020-09-04 15:25:36 -0500

    styling for list and card

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavBar } from './nav/NavBar';
 import { ApplicationViews } from './ApplicationViews';
 import { Header } from './header/Header';
+import './KandyKorner.css';
 
 export const KandyKorner = props => {
   return (
