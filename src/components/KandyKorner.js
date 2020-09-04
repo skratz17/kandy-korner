37d23de60088238578eb339623aae2cc0ba7ed7f
Jacob Eckert <jweckert17@gmail.com>
2020-09-04 15:20:42 -0500

    styling for header and navbar

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavBar } from './nav/NavBar';
 import { ApplicationViews } from './ApplicationViews';
+import { Header } from './header/Header';
 
 export const KandyKorner = props => {
   return (
     <>
+      <Header />
       <NavBar />
       <ApplicationViews />
     </>
