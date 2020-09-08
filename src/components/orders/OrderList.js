e331ec56ee837fab662cc18eb09784c7cb037f93
Jacob Eckert <jweckert17@gmail.com>
2020-09-08 14:12:28 -0500

    collapsing order table on product variant, showing quantity of product in order, showing unit price and total price in order table

--- src/components/orders/OrderList.js
+++ src/components/orders/OrderList.js
@@ -13,18 +13,35 @@ export const OrderList = props => {
     getProducts();
   }, []);
 
-  const productsOrdered = orders
+  const productsOrderedCounts = orders
     .filter(o => o.customerId === parseInt(localStorage.getItem('kandy_customer')))
-    .map(o => products.find(p => p.id === o.productId) || {});
+    .map(o => products.find(p => p.id === o.productId) || {})
+    .reduce((counts, product) => {
+      counts[product.id] = counts[product.id] ? counts[product.id] + 1 : 1
+      return counts;
+    }, {});
+
+  const collapsedProductsOrdered = Object.keys(productsOrderedCounts)
+    .map(id => {
+      const product = products.find(p => p.id === parseInt(id)) || {};
+
+      return { 
+        ...product,
+        count: productsOrderedCounts[id],
+        totalPrice: product.price * productsOrderedCounts[id] 
+      };
+    });
 
   const tableConfig = [
     { header: 'Candy', field: 'name' },
-    { header: 'Price', field: 'price' }
+    { header: 'Quantity', field: 'count' },
+    { header: 'Price/unit', field: 'price' },
+    { header: 'Total Price', field: 'totalPrice' }
   ];
 
   return (
     <div className="orderListWrapper">
-      <Table config={tableConfig} items={productsOrdered} />
+      <Table config={tableConfig} items={collapsedProductsOrdered} />
     </div>
   );
 };
