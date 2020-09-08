87d1b13439f0a40205086cf5702f3f9ab0a8a80c
Jacob Eckert <jweckert17@gmail.com>
2020-09-08 15:37:09 -0500

    removed console log, made one-statement component function

--- src/components/ui/components/Table.js
+++ src/components/ui/components/Table.js
@@ -2,20 +2,17 @@ import React from 'react';
 
 import './Table.css';
 
-export const Table = ({ config, items }) => {
-  console.log('hi table', items);
-  return (
-    <table className="table">
-      <thead className="tableHead">
-        <tr className="table__row tableHead__row">
-          { config.map(c => <th key={c.field} className="table__cell tableHead__cell">{c.header}</th>) }
-        </tr>
-      </thead>
-      <tbody className="tableBody">
-        { items.map(i => (
-          <tr key={i.id} className="table__row tableBody__row">{config.map(c => <td key={i.id + '--' + c.field} className="table__cell tableBody__cell">{i[c.field]}</td>)}</tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+export const Table = ({ config, items }) => (
+  <table className="table">
+    <thead className="tableHead">
+      <tr className="table__row tableHead__row">
+        { config.map(c => <th key={c.field} className="table__cell tableHead__cell">{c.header}</th>) }
+      </tr>
+    </thead>
+    <tbody className="tableBody">
+      { items.map(i => (
+        <tr key={i.id} className="table__row tableBody__row">{config.map(c => <td key={i.id + '--' + c.field} className="table__cell tableBody__cell">{i[c.field]}</td>)}</tr>
+      ))}
+    </tbody>
+  </table>
+);
