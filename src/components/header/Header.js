01fc6d3cc83481a23642dc9b799de3e7095f3388
Jacob Eckert <jweckert17@gmail.com>
2020-09-08 12:03:23 -0500

    implemented login/register logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,6 @@ import './Header.css';
 export const Header = props => (
   <header className="header">
     <h1 className="header__text">Kandy Korner</h1>
-    <p className="header__slogan">If we had any more candy, we'd be Voltaire, cause we'd be Candide, (like, "candied"), are we rite??</p>
+    <p className="header__slogan">If we had any more candy, we'd be Voltaire, cause we'd be Candide, (like, "candied"), amirite??</p>
   </header>
 );
