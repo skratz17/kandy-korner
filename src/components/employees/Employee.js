8f266c527419a4a55bf755b61ba052a049a3babf
Jacob Eckert <jweckert17@gmail.com>
2020-09-09 14:15:18 -0500

    deleting employees

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const Employee = ({ employee }) => (
+export const Employee = ({ employee, onFireEmployee }) => (
   <article className="card employee">
     <h3 className="card__header employee__name">{employee.name}</h3>
     <div className="card__contentWrapper">
+      <button className="btn btn--delete" onClick={() => onFireEmployee(employee.id)}>Fire Employee</button>
     </div>
   </article>
 );
